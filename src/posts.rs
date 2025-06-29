94d58ddbc19a28872026ebf8b4dcfc22920d6681
rusty_person <218080797+ashleylolol@users.noreply.github.com>
2025-06-29 14:27:17 +0530

    Code is broken..

---
+++ src/posts.rs
@@ -0,0 +1,14 @@
+use serde::{Deserialize, Serialize};
+use chrono::Utc;
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Post{
+    pub content: String,
+    pub timestamp: chrono::DateTime<Utc>,
+}
+
+pub fn create_posts(content: String){
+    Post{
+        content,
+        timestamp: Utc::now(),
+    }
+}
